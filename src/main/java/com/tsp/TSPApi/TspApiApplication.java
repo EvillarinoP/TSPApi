f9a72121b47667e4a5f8438f1ad3e151f2dce662
EvillarinoP <enrique.villarinoperez@gmail.com>
2021-09-29 19:16:15 +0200

    Scaffolding of Springboot application.

---
+++ src/main/java/com/tsp/TSPApi/TspApiApplication.java
@@ -0,0 +1,13 @@
+package com.tsp.TSPApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TspApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TspApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsp/TSPApi/TspApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsp.TSPApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TspApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
