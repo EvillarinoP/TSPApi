bc0f4904e557d548064b250338195bb5c79425f6
enrique <enrique.villarino@logicalglue.com>
2024-09-30 12:22:27 +0200

    Added helm chart and deployment and service for the restui app.

--- src/main/java/com/tsp/TSPApi/Presentation/RouteController.java
+++ src/main/java/com/tsp/TSPApi/Presentation/RouteController.java
@@ -5,6 +5,7 @@ import com.tsp.TSPApi.Application.IRouteApplicationService;
 import com.tsp.TSPApi.Entities.DTOs.RouteResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +16,14 @@ public class RouteController {
     private IRouteApplicationService _routeApplicationService;
 
     @GetMapping(value="/routeGA")
+    @CrossOrigin
     public RouteResponse calculateRouteGA() {
 
         return _routeApplicationService.calculateRouteGA();
     }
 
     @GetMapping("/liveness")
+    @CrossOrigin
     public ResponseEntity livenessMethod()
     {
         return ResponseEntity.ok("Healthy!");
